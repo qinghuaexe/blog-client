9e5ea30c8c4aa635329bfab95e7543ccb8c2303c
qinghuaexe <qinghuaexe@gmail.com>
2019-03-20 19:19:18 +0800

    写静态页面

---
+++ src/components/Footer.vue
@@ -0,0 +1,17 @@
+<template>
+  <footer>
+    <p>xiedaimala.com 2019 wechat:xiedaimala01</p>
+  </footer>
+</template>
+
+<style lang="less">
+
+footer {
+  align-self: end;
+  background: #d7d7d7;
+  color: #666;
+  font-size: 13px;
+  padding: 10px;
+  text-align: center;
+}
+</style>
